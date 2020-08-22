db0d60a3531dce35319f8809c40e54153ce5bf23
pmAndrian <66482484+pmAndrian@users.noreply.github.com>
2020-08-22 19:52:50 +0300

    Updates

---
+++ precache-manifest.7ec7c7603d1a13046a4015a617112ce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9527b120c3e2e6f0237311645e821bf4",
+    "url": "/brainbook/index.html"
+  },
+  {
+    "revision": "4771951ce2b2eea9c10d",
+    "url": "/brainbook/static/css/main.83194d2c.chunk.css"
+  },
+  {
+    "revision": "9fda121abcfbd5a7e59f",
+    "url": "/brainbook/static/js/2.aeeeff46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brainbook/static/js/2.aeeeff46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4771951ce2b2eea9c10d",
+    "url": "/brainbook/static/js/main.acf109be.chunk.js"
+  },
+  {
+    "revision": "1c39140038f6c8e8f56e",
+    "url": "/brainbook/static/js/runtime-main.796700b5.js"
+  },
+  {
+    "revision": "3251c62eac2b87da8e90627dcf580e57",
+    "url": "/brainbook/static/media/banner.3251c62e.jpg"
+  }
+]);
